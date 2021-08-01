85d75f134d1218d8d1934554c877e3afc696cf0c
Juliane Marques <mqs.juliane@gmail.com>
2021-08-01 10:19:34 -0300

    First commit to add some code workig with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Ir ao medico",
+      day: "Feb5th at 2:30pm",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Ir ao mercado",
+      day: "March 5th at 6:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Ir na vovo",
+      day: "Feb 6th at 4:30pm",
+      reminder: true,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello Jules</h1>
+      <Header />
+      {tasks.lenght > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No tasks =)"
+      )}
     </div>
   );
 }
